d9f4e4d341af281ace4fa7cb8c4dac5ced28e27d
thibaulthans <thibault.hans@hei.fr>
2017-04-11 11:28:34 +0200

    debut

---
+++ projethccgcabackoffice/js/globaljs.js
@@ -0,0 +1,22 @@
+window.onload=function(){
+
+ document.getElementById("href_ajouter").onclick = function() {
+
+    location.href='modification_ajouter.html';
+
+ };
+
+ document.getElementById("href_supprimer").onclick = function() {
+
+    location.href='modification_supprimer.html';
+
+ };
+
+ document.getElementById("href_modifier").onclick = function() {
+
+    location.href='modification_modifier.html';
+
+ };
+
+
+}
